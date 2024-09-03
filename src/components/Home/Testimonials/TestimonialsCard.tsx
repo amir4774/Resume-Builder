c9bcfe62504cecae16c2c7a65ba9d600a3c4fdcc
amir4774 <amirabbassi1382@gmail.com>
2024-09-03 17:01:12 +0330

    selection and user image sizes

--- src/components/Home/Testimonials/TestimonialsCard.tsx
+++ src/components/Home/Testimonials/TestimonialsCard.tsx
@@ -11,7 +11,7 @@ const TestimonialsCard = ({
     <div className="bg-white shadow-lg rounded-lg p-4 max-w-[500px] min-h-[230px] dark:bg-dark_bg">
       <div className="flex flex-wrap justify-center items-center space-x-4 space-y-4 md:justify-start">
         <div className="rounded-full size-24 relative">
-          <Image src={profile} alt="Profile" fill />
+          <Image src={profile} alt="Profile" fill sizes="(100vw, 100vh)" />
         </div>
 
         <div className="font-bold">
